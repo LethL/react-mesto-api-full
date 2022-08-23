5ad56648769f4132865cdc64b056f997df5dfcbb
Leth4L <carribean_08@mail.ru>
2022-08-23 12:03:54 +0300

    refactor: change base url on frontend

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://https://api.dmitry.mesto.nomoredomains.sbs/';
 
 function handleResponse(res) {
     if (res.ok) {
