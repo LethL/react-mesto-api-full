56125944cdb399123731e46bf6ad526eeca647c1
Leth4L <carribean_08@mail.ru>
2022-08-23 12:56:40 +0300

    feat: add CORS

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,12 @@ const { userValidation, loginValidation } = require('./middlewares/validation');
 const handlerErrors = require('./middlewares/handlerErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'https://dmitry.mesto.nomoredomains.sbs',
+  'http://dmitry.mesto.nomoredomains.sbs',
+  'localhost:3000',
+];
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -17,6 +23,26 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.status(200).send();
+  }
+
+  next();
+});
 
 app.use(requestLogger);
 
