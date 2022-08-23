d8ed0f6f1a7b91b27a9e8f800a01de96699d1cee
Leth4L <carribean_08@mail.ru>
2022-08-23 13:47:19 +0300

    fix: fixed require dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-require('dotenv').config();
 const router = require('./routes/router');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
